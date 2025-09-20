b11c546af61e6e2eeded72c706d6296089a8ab6d
Fomas <mujik@test.ui>
2025-09-20 12:37:24 +0700

    Изменено: в класс окружности добавлены поля и конструктор класса

--- Circle.cs
+++ Circle.cs
@@ -1,4 +1,13 @@
 public class Circle
 {
+    private int _x, _y, _radius;
+
+    public Circle(int x, int y, int radius)
+    {
+        _x = x;
+        _y = y;
+        _radius = radius;
+    }
+
     
 }
