171bb8e597acc87a1d3d690f5ef69669fe814a13
Fomas <mujik@test.ui>
2025-09-20 12:12:12 +0700

    Изменено: добавил комментарии  к коду

--- Shape.cs
+++ Shape.cs
@@ -2,12 +2,20 @@ public class Shape
 {
 
     #region Общие поля
+
+    //Для хранения типа мы сейчас специально используем "опасную" строку
     public string Type { get; set; }
+
+    //Х и У тут хранить можно
     public int X { get; set; }
     public int Y { get; set; }
 
     #endregion
 
+    //А вот эти переменные мы смешали в кучу
+    //Их нужно будет разделить с поомщью инкапсулияции
+    //Радиус - для круга
+    // Высота и ширина для прямоугольника
     public int Radius { get; set; }
     public int Height { get; set; }
     public int Width { get; set; }
