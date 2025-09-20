4467c6c920b41499d18893259584bfd726aaac7e
Fomas <mujik@test.ui>
2025-09-20 12:22:54 +0700

    Добавил: создание объектов фигур и вызов отрисовщика

--- Program.cs
+++ Program.cs
@@ -4,6 +4,32 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        GraphicEditor graphicEditor = new GraphicEditor();
+
+        List<Shape> shapes = new List<Shape>();
+
+        //Использование типов как строк требует 
+        //большого внимания к написанию
+        //я сам пока делал пример забыл, что в методе-рисовальщике 
+        //я написал типы с маленькой буквы :)
+
+        Shape rectangle = new Shape()
+        {
+            Type = "rectangle", // Тут мы обращаемся к каждому свойству
+            X = 15,             // и задаем им значения
+            Y = 20,             
+            Height = 50,
+            Width = 33
+        };
+        Shape rectangle_bad = new Shape() { Type = "Rectangle" };
+        Shape circle = new Shape() { Type = "Circle" };
+        Shape circle_bad = new Shape() { Type = "Cricle" };
+
+        shapes.Add(rectangle);
+        shapes.Add(circle);
+        shapes.Add(circle_bad);
+
+        graphicEditor.DrawAllShapes(shapes);
+
     }
 }
