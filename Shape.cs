aae8351af477a85ed5f12e6a7749014d32c662bd
Fomas <mujik@test.ui>
2025-09-20 11:45:13 +0700

    Добавил: общие поля для всех фигур в одном классе

---
+++ Shape.cs
@@ -0,0 +1,14 @@
+public class Shape
+{
+
+    #region Общие поля
+    public string Type { get; set; }
+    public int X { get; set; }
+    public int Y { get; set; }
+
+    #endregion
+
+    public int Radius { get; set; }
+    public int Height { get; set; }
+    public int Width { get; set; }
+}
