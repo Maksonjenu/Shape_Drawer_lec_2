16e96456e8ce99fb346a1305feb7a31ad23cda07
Fomas <mujik@test.ui>
2025-09-20 12:42:09 +0700

    Изменено: реализован конструктор класса прямоугольника

--- Rectangle.cs
+++ Rectangle.cs
@@ -4,6 +4,9 @@ public class Rectangle
 
     public Rectangle(int x, int y, int width, int height)
     {
-        
+        _x = x;
+        _y = y;
+        _width = width;
+        _height = height;
     }
 }
