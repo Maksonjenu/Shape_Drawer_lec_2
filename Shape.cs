f33645adbf2e0c60d4880e78c2ee7b3644ac12cf
Maksonjenu <genius690@mail.ru>
2025-09-22 18:23:23 +0700

    Удалены неиспользуемые свойства и комментарии из класса Shape

--- Shape.cs
+++ Shape.cs
@@ -1,18 +1,6 @@
 public class Shape
 {
 
-    //На текущем этапе класс фигуры не используется
-    //Далее мы будем использовать его в качестве родительского
-
-
-    #region Общие поля
-    public string Type { get; set; }
-    public int X { get; set; }
-    public int Y { get; set; }
-
-    public int Radius { get; set; }
-    public int Height { get; set; }
-    public int Width { get; set; }
-
-    #endregion
+    // protected - видно нам и нашим наследникам.
+    protected int _x, _y;
 }
