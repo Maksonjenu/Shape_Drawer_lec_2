2b9846cefe4de98eefef3e681b529acce4670ba5
Maksonjenu <genius690@mail.ru>
2025-09-22 18:27:57 +0700

    Добавлены комментарии для улучшения понимания класса Rectangle

--- Rectangle.cs
+++ Rectangle.cs
@@ -1,9 +1,16 @@
+
+// Наследуемся от класса Shape
 public class Rectangle : Shape
 {
+
+    // Поля, специфичные для прямоугольника
     private int _width, _height;
 
     public Rectangle(int x, int y, int width, int height)
     {
+
+        // Поля _x и _y теперь принадлежат базовому классу Shape,
+        // поэтому мы можем к ним обращаться напрямую
         _x = x;
         _y = y;
         _width = width;
