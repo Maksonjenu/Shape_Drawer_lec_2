174286cdaeea1901b74537f6e1b2f5ca4f85cf58
Maksonjenu <genius690@mail.ru>
2025-09-22 18:23:10 +0700

    Изменено: обновлен тип списка фигур на List<Shape> для повышения безопасности типов

--- Program.cs
+++ Program.cs
@@ -6,14 +6,8 @@ class Program
     {
         GraphicEditor graphicEditor = new GraphicEditor();
 
-        List<object> shapes = new List<object>();
+        List<Shape> shapes = new List<Shape>();
 
-        // Теперь наш список фигур имеет базовый тип 
-        // object. В данный список можно добавлять объекты
-        // других типов, это небезопасно.
-
-        // Также мы создаем наши переменные как объекты
-        // созданных нами классов
         Rectangle rectangle = new Rectangle(15, 20, 33, 50);
         Rectangle rectangle_bad = new Rectangle(0, 0, 0, 0);
         Circle circle = new Circle(0, 0, 10);
