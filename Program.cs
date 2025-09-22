1d2d97c425bac24fa43661e4e1eaf6027a8d204d
Maksonjenu <genius690@mail.ru>
2025-09-22 18:27:51 +0700

    Изменено: добавлены комментарии для улучшения понимания кода в методе Main

--- Program.cs
+++ Program.cs
@@ -6,8 +6,13 @@ class Program
     {
         GraphicEditor graphicEditor = new GraphicEditor();
 
+        // Возвращаем тип данных Shape в словарь
+        // Теперь мы можем создавать список фигур
+        // и добавлять туда только объекты классов-наследников
+
         List<Shape> shapes = new List<Shape>();
 
+        //Объявление конкретных фигур осталось без изменений
         Rectangle rectangle = new Rectangle(15, 20, 33, 50);
         Rectangle rectangle_bad = new Rectangle(0, 0, 0, 0);
         Circle circle = new Circle(0, 0, 10);
