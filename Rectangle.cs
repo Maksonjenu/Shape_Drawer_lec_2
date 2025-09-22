7446e6ceb9152c46e22e572cc4b57365ae525e92
Maksonjenu <genius690@mail.ru>
2025-09-22 18:23:16 +0700

    Изменено: добавлено наследование класса Rectangle от класса Shape

--- Rectangle.cs
+++ Rectangle.cs
@@ -1,6 +1,6 @@
-public class Rectangle
+public class Rectangle : Shape
 {
-    private int _x, _y, _width, _height;
+    private int _width, _height;
 
     public Rectangle(int x, int y, int width, int height)
     {
