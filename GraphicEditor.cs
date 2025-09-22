41fe0db61976a9e50aeb8f2815afbb1896b9c2f9
Fomas <mujik@test.ui>
2025-09-22 14:58:00 +0700

    Изменено: добавлены коментарии к классу ГрафРедактора

--- GraphicEditor.cs
+++ GraphicEditor.cs
@@ -2,14 +2,24 @@ public class GraphicEditor
 {
 
     //Метод для отрисовки всех фигур из списка
+    // Сигнатура метода изменилась, теперь аргументом
+    // является список "объектов"
     public void DrawAllShapes(List<object> shapes)
     {
         foreach (var shape in shapes)
         {
+            // С помощью оператора is мы проверяем
+            // допустимость преобразования shape
+            // в нужный нам тип, и если это возможно
+            // преобразование сохраняется в rect
             if (shape is Rectangle rect)
             {
                 rect.Draw();
             }
+
+            // Аналогично коду выше, только
+            // с классом окружности
+
             else if (shape is Circle circle)
             {
                 circle.Draw();
