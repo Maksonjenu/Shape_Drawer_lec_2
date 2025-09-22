131772a3149a66157e9f10edea44475907006ec7
Maksonjenu <genius690@mail.ru>
2025-09-22 18:27:38 +0700

    Изменено: добавлены комментарии для улучшения понимания класса Circle

--- Circle.cs
+++ Circle.cs
@@ -1,9 +1,13 @@
+// Наследуемся от класса Shape
 public class Circle : Shape
 {
+    // Поля, специфичные для круга
     private int _radius;
 
     public Circle(int x, int y, int radius)
     {
+        // Поля _x и _y теперь принадлежат базовому классу Shape,
+        // поэтому мы можем к ним обращаться напрямую
         _x = x;
         _y = y;
         _radius = radius;
